7f5ef82a53e98bb4b91e8f4ccd49fd1fbfe838ce
xqill275 <oliverlong275@gmail.com>
2025-01-30 15:13:07 +0000

    finished people part of the dashboard

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ exports.login = (req, res) => {
     fakeUsername = "TestUser"
     fakePassword = "1234"
     if (username === fakeUsername && password === fakePassword ) {
-        res.redirect('/admin-panel');
+        res.redirect('/dashBoard');
     } else {
         return res.render('login', {
             message: 'Wrong username or password. Please try again.'
---
+++ public/javaScript/dashboard.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active'); // Toggle the 'active' class to show or hide the nav
+});
